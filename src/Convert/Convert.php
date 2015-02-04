6b46de2a002f7f9557b70bc49b8e3651608579a3
Jonathan Foote <jf@jonathanfoote.com>
2015-02-04 12:03:45 -0500

    #14 Experimental, use a buffer when writing pov-inc file

--- src/Convert/Convert.php
+++ src/Convert/Convert.php
@@ -12,6 +12,8 @@ abstract class Convert {
 	 */
 	protected $Render3d;
 
+	protected $fwriteBuffers = [];
+
 	/**
 	 * Constructor gonna construct.
 	 * 
@@ -20,6 +22,36 @@ abstract class Convert {
 	 */
 	public function __construct(\Libre3d\Render3d\Render3d $render3d) {
 		$this->Render3d = $render3d;
+	}
+
+	/**
+	 * Write to a file, using a buffer size, to reduce number of writes to a file.
+	 * 
+	 * This essencially implements our own file write buffer, as the one built in is not always reliable.  Doing this
+	 * way shaves time off of writing larger files.
+	 * 
+	 * To write remaining buffer to the file, or to simply not use buffering, pass in 0 for $bufferSize.
+	 * 
+	 * @param resource $handle
+	 * @param string $contents Contents to write to file
+	 * @param string $fn Filename being written, only used as array key to store the buffer
+	 * @param integer $bufferSize Set to 0 to write any remaining in the buffer to the file
+	 * @return void
+	 */
+	protected function fwriteBuffer($handle, $contents, $fn, $bufferSize = 8000) {
+		if (!isset($this->fwriteBuffers[$fn])) {
+			$this->fwriteBuffers[$fn] = '';
+		}
+		$this->fwriteBuffers[$fn] .= $contents;
+		if (strlen($this->fwriteBuffers[$fn]) > $bufferSize) {
+			if ($bufferSize) {
+				fwrite($handle, $this->fwriteBuffers[$fn], $bufferSize);
+				$this->fwriteBuffers[$fn] = substr($this->fwriteBuffers[$fn], $bufferSize);
+			} else {
+				fwrite($handle, $this->fwriteBuffers[$fn]);
+				$this->fwriteBuffers[$fn] = '';
+			}
+		}
 	}
 
 	/**
